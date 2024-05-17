db197bfe8306bcae0d0d5a6247011592d32256e5
dannyjesus22 <zambranomeradannyjesus@gmail.com>
2024-05-17 13:55:40 -0500

    first commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+import datetime
+from cloudinary.models import CloudinaryField
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    description = models.TextField()
+    image = CloudinaryField(upload_to='image')
+    date = models.DateField(datetime.date.today)
+    
+# Create your models here.
