e5ae5f9dac3fad468b4622c1e9ce6e0eb8e0d780
Dannyzm22 <132167729+Dannyzm22@users.noreply.github.com>
2024-05-17 14:26:17 -0500

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from cloudinary.models import CloudinaryField
 class Post(models.Model):
     title=models.CharField(max_length=100)
     description = models.TextField()
-    image = CloudinaryField(upload_to='image')
+    image = CloudinaryField('image')
     date = models.DateField(datetime.date.today)
     
 # Create your models here.
